054e61795c8e72463862498af80e43f7e6aec86a
Joao Gabriel Franco <80074087+JoaoGabrielSantosFranco@users.noreply.github.com>
2023-04-18 15:35:11 -0300

    atualizando

--- src/app/component.js
+++ src/app/component.js
@@ -1,12 +1,12 @@
 "use client";
 import styles from './page.module.css'
 import { useState } from 'react';
-import { Button } from 'reactstrap';
 export function Component({ data }) {
 
     const [start, setStart] = useState(0);
     const [end, setEnd] = useState(2);
     const [hoverId, setHoverId] = useState(null); // Estado para controlar o hover do mouse
+    const [tasks, setTasks] = useState(data);
 
     // quando clica faz a lista voltar 1 pra cada bloco
     const handlePrevClick = () => {
@@ -18,8 +18,7 @@ export function Component({ data }) {
 
     // quando clica faz a lista voltar 1 pra cada bloco
     const handleNextClick = () => {
-
-        if (end < data.length - 1) {
+        if (end < tasks.length - 1) {
             setStart(start + 1);
             setEnd(end + 1);
         }
@@ -36,39 +35,55 @@ export function Component({ data }) {
         setHoverId(null);
     };
 
+    const task_done = (a) => {
+        console.log(tasks)
+        console.log(tasks[0].id === a)
+
+        setTasks(tasks.filter(tasks => tasks.id != a))
+        handlePrevClick();
+    };
+
     return (
         <main className={styles.background}>
             <div>
 
                 <div className={styles.titleDiv}>
-                    <p className={styles.title}>Cursos de Programação</p>
+                    <p className={styles.title}>Tasks</p>
                 </div>
 
                 <div className={styles.boxContent}>
 
-                    <button  className="danger" onClick={handlePrevClick}>volta</button>
+                    <button className={styles.button} onClick={handlePrevClick}>{"<"}</button>
 
                     <ul className={styles.boxContent} >
-                        {data.slice(start, end + 1).map((item) => ( //para cada item entre o start e end:
+                        {tasks.slice(start, end + 1).map((item) => ( //para cada item entre o start e end:
+                            <div className={styles.boxContentIndividual}>
+                                <li className={styles.content}
+                                    key={item.id}
+                                    onMouseEnter={() => handleMouseEnter(item.id)}
+                                    onMouseLeave={handleMouseLeave}>
 
-                            <li className={styles.content}
-                                key={item.id}
-                                onMouseEnter={() => handleMouseEnter(item.id)}
-                                onMouseLeave={handleMouseLeave}>
-
-                                {item.titulo}
-                                {hoverId === item.id && (// se hover for igual id, ou seja se o mause estiver em cima deste elemento;
-                                    <div>
-                                        <p>Descrição: {item.descricao}</p>
-                                    </div>
-                                )}
-
-                            </li>
+                                    {item.titulo}
+                                    {hoverId === item.id && (// se hover for igual id, ou seja se o mause estiver em cima deste elemento;
+                                        <div>
+                                            <p>{item.id}</p>
+                                            <p>Descrição: {item.descricao}</p>
+                                            <button className={styles.button} onClick={() => task_done(item.id)}>{"Feita"}</button>
+                                        </div>
+                                    )}
+                                </li>
+                            </div>
                         ))}
+                        {tasks.length == 0 && (
+                            <div>
+                                <p>Voce não tem mais Tasks Por Hoje</p>
+                            </div>
+                        )}
                     </ul>
-
-                    <button onClick={handleNextClick}>avanca</button>
-
+                    <button className={styles.button} onClick={handleNextClick}>{">"}</button>
+                </div>
+                <div className={styles.counter}>
+                    <p>{end - 1}/{tasks.length - 2}</p>
                 </div>
             </div>
         </main>
