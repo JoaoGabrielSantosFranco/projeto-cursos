87329b890f3bd74673007d5318b573cdb0746e58
Joao Gabriel Franco <80074087+JoaoGabrielSantosFranco@users.noreply.github.com>
2023-04-18 10:46:26 -0300

    adicionando estado hover quando passa por cima dos itens

--- src/app/component.js
+++ src/app/component.js
@@ -5,7 +5,9 @@ export function Component({ data }) {
 
     const [start, setStart] = useState(0);
     const [end, setEnd] = useState(2);
+    const [hoverId, setHoverId] = useState(null); // Estado para controlar o hover do mouse
 
+    // quando clica faz a lista voltar 1 pra cada bloco
     const handlePrevClick = () => {
         if (start > 0) {
             setStart(start - 1);
@@ -13,33 +15,62 @@ export function Component({ data }) {
         }
     };
 
+    // quando clica faz a lista voltar 1 pra cada bloco
     const handleNextClick = () => {
-        console.log(data.length);
-        console.log(end);
+
         if (end < data.length - 1) {
             setStart(start + 1);
             setEnd(end + 1);
         }
-
     };
+
+
+    // quando o mause passa por cima Atualiza o ID do hover
+    const handleMouseEnter = (id) => {
+        setHoverId(id);
+    };
+
+    // quando o mause passa por cima remove o ID do hover
+    const handleMouseLeave = () => {
+        setHoverId(null);
+    };
+
     return (
         <main className={styles.background}>
-            {data.map(_ => JSON.stringify(_))}
-            <div >
+            <div>
+
                 <div className={styles.titleDiv}>
                     <p className={styles.title}>CURSO DE PROGRAMAÇÃO</p>
                 </div>
 
                 <div className={styles.boxContent}>
+
                     <button onClick={handlePrevClick}>volta</button>
-                    <ul className={styles.content}>{data[start].titulo}</ul>
-                    <ul className={styles.content}>{data[end - 1].titulo}</ul>
-                    <ul className={styles.content}>{data[end].titulo}</ul>
+
+                    <ul className={styles.boxContent} >
+                        {data.slice(start, end + 1).map((item) => ( //para cada item entre o start e end:
+
+                            <li className={styles.content}
+                                key={item.id}
+                                onMouseEnter={() => handleMouseEnter(item.id)}
+                                onMouseLeave={handleMouseLeave}>
+
+                                {item.titulo}
+                                {hoverId === item.id && (// se hover for igual id, ou seja se o mause estiver em cima deste elemento;
+                                    <div>
+                                        <p>Descrição: {item.descricao}</p>
+                                    </div>
+                                )}
+
+                            </li>
+                        ))}
+                    </ul>
+
                     <button onClick={handleNextClick}>avanca</button>
+
                 </div>
             </div>
         </main>
-
-
     )
 }
+
