8c7bd718118bdb0fd8c590a5f9cecb2e0d9101b4
Joao Gabriel Franco <80074087+JoaoGabrielSantosFranco@users.noreply.github.com>
2023-04-20 14:25:17 -0300

    separando componentes

---
+++ src/app/components/task.js
@@ -0,0 +1,31 @@
+import styles from 'src/app/page.module.css'
+import { useState } from 'react';
+export function Task({ task, taskDone, }) {
+
+    const [hover,setHover] = useState(false);
+
+    const handleMouseEnter = () => {
+        setHover(true);
+    };
+
+    const handleMouseLeave = () => {
+        setHover(false);
+    };
+
+    return (
+        <div  className={styles.boxContentIndividual}>
+            <li className={styles.content}
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}>
+                {task.titulo}
+                {hover && (
+                    <div>
+                        <p>{task.id}</p>
+                        <p>Descrição: {task.descricao}</p>
+                        <button className={styles.button} onClick={() => taskDone(task.id)}>{"Feita"}</button>
+                    </div>
+                )}
+            </li>
+        </div>
+    )
+}
--- src/app/components/tasks.js
+++ src/app/components/tasks.js
@@ -1,14 +1,13 @@
 "use client";
 import styles from 'src/app/page.module.css'
 import { useState } from 'react';
+import { Task } from './task.js';
 
 export function Tasks({ data }) {
     const [start, setStart] = useState(0);
     const [end, setEnd] = useState(2);
-    const [hoverId, setHoverId] = useState(null); // Estado para controlar o hover do mouse
     const [tasks, setTasks] = useState(data);
 
-    // quando clica faz a lista voltar 1 pra cada bloco
     const handlePrevClick = () => {
         if (start > 0) {
             setStart(start - 1);
@@ -16,7 +15,6 @@ export function Tasks({ data }) {
         }
     };
 
-    // quando clica faz a lista voltar 1 pra cada bloco
     const handleNextClick = () => {
         if (end < tasks.length - 1) {
             setStart(start + 1);
@@ -24,21 +22,10 @@ export function Tasks({ data }) {
         }
     };
 
-    // quando o mause passa por cima Atualiza o ID do hover
-    const handleMouseEnter = (id) => {
-        setHoverId(id);
-    };
-
-    // quando o mause passa por cima remove o ID do hover
-    const handleMouseLeave = () => {
-        setHoverId(null);
-    };
-
-    const task_done = (a) => {
+    const taskDone = (a) => {
         setTasks(tasks.filter(tasks => tasks.id != a))
         handlePrevClick();
     };
-
     return (
         <main>
             <div className={styles.boxContent}>
@@ -46,22 +33,8 @@ export function Tasks({ data }) {
                 <button className={styles.button} onClick={handlePrevClick}>{"<"}</button>
 
                 <ul className={styles.boxContent} >
-                    {tasks.slice(start, end + 1).map((item) => ( //para cada item entre o start e end:
-                        <div key={item.id} className={styles.boxContentIndividual}>
-                            <li className={styles.content}
-                                onMouseEnter={() => handleMouseEnter(item.id)}
-                                onMouseLeave={handleMouseLeave}>
-
-                                {item.titulo}
-                                {hoverId === item.id && (// se hover for igual id, ou seja se o mause estiver em cima deste elemento;
-                                    <div>
-                                        <p>{item.id}</p>
-                                        <p>Descrição: {item.descricao}</p>
-                                        <button className={styles.button} onClick={() => task_done(item.id)}>{"Feita"}</button>
-                                    </div>
-                                )}
-                            </li>
-                        </div>
+                    {tasks.slice(start, end + 1).map((task) => ( //para cada item entre o start e end:
+                        <Task task={task} taskDone={taskDone} key={task.id} />
                     ))}
                     {tasks.length == 0 && (
                         <div>
